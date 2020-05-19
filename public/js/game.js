71f079a1caa60172b3bc40f398f818d9bd0ff8fb
Gerardo <gerardo.cardosa@gmail.com>
2020-05-19 00:03:47 -0700

    feat: Added user events
    Now a user can move the snake in all directions.

--- public/js/game.js
+++ public/js/game.js
@@ -2,17 +2,46 @@ const cellZise = 20;
 const boardSize = 10;
 
 let snake;
-
-function setup(){
-    const side = cellZise * boardSize;
-    const half = parseInt(side/2);
+const side = cellZise * boardSize;
+const half = side/2;
+let counter = 0;
+function setup(){   
     createCanvas(side, side);
     snake = new Snake();
-    snake.start(half,half);
-    frameRate(1);
+    snake.start(half,half, cellZise);
+    //frameRate(1);
 }
 
 function draw(){
-    background(200)
-    snake.update(cellZise);    
+    if(counter >= 15){    
+        background(200);
+        boardLines(); 
+        snake.update(cellZise, side);   
+        counter =0;       
+    }
+    userInput(); 
+    counter++;
+}
+
+function boardLines(){
+    for(let row=0; row <= side; row+=cellZise){
+        line(0,row,side, row);
+        line(row,0,row, side);
+    }
+}
+
+function userInput(){
+    if(keyIsDown(UP_ARROW)){
+        //console.log('UP');
+        snake.dir = 'up';
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+        snake.dir = 'right';
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        snake.dir = 'down';
+    }
+    if(keyIsDown(LEFT_ARROW)){
+        snake.dir = 'left';
+    }
 }
--- public/js/snake.js
+++ public/js/snake.js
@@ -2,26 +2,40 @@ class Snake{
     constructor(){
         this.body = [];
         this.dir = 'right';
+        this.zise = 0;
     }
 
-    start(x,y){
+    start(x,y, size){
         this.body.push(new Part(x,y));
+        this.size = size;
     }
 
-    update(cell){
+    update(cell, max){
         let head = this.body[this.body.length -1];
         if(this.dir=='right'){
-            this.body.push(new Part(head.x + cell, head.y));
+            let next = head.x + cell >= max? 0: head.x + cell;
+            this.body.push(new Part(next, head.y));
+        }
+        if(this.dir=='up'){
+            let next = head.y - cell <0? max - cell: head.y - cell;
+            this.body.push(new Part(head.x, next));
+        }
+        if(this.dir=='left'){
+            let next = head.x - cell <0? max - cell: head.x - cell;
+            this.body.push(new Part(next, head.y));
+        }
+        if(this.dir=='down'){
+            let next = head.y + cell >=max? 0: head.y + cell;
+            this.body.push(new Part(head.x, next));
         }
 
         this.body.shift();
-
         this.draw();
     }
 
     draw(){
         for(let [key, part] of this.body.entries()){
-            circle(part.x, part.y, 20);
+            square(part.x, part.y, this.size);
         }
     }
 }
